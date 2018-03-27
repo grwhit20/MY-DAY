f700f022402ed395061ae9c8c2cbb8b01d6d7075
August Whitaker <grwhit20@gmail.com>
2018-03-27 02:09:28 -0400

    First upload

---
+++ att/java.js
@@ -0,0 +1,84 @@
+function hourOneClick() {
+  document.getElementById("infophoto").style.background = "url('https://i.imgur.com/XMerWpH.jpg')";
+  document.getElementById("infocontainer").style.transform = "translateY(0%)";
+  document.getElementById("infoheader").innerHTML = "HOUR 1";
+  document.getElementById("infobody").innerHTML = "Get out of bed.";
+  // document.getElementById("hr1").style['border-radius'] = "25px";
+}
+
+ function hourTwoClick() {
+   document.getElementById("infophoto").style.background = "url('https://i.imgur.com/kk6Djfw.jpg')";
+   document.getElementById("infocontainer").style.transform = "translateY(0%)";
+   document.getElementById("infoheader").innerHTML = "HOUR 2";
+   document.getElementById("infobody").innerHTML = "After showering and getting dressed for the day, I always make my bed. A lot of people have their own little things they do every single day as part of their routine.</br> </BR>In some way you can call it self-care because there's nothing better than getting into a made bed at night and passing out. It’s nothing much but it takes very little time in the morning and has great payoff.";
+}
+
+ function hourThreeClick() {
+   document.getElementById("infophoto").style.background = "url('https://i.imgur.com/CgHLjmW.jpg')";
+   document.getElementById("infocontainer").style.transform = "translateY(0%)";
+   document.getElementById("infoheader").innerHTML = "HOUR 3";
+   document.getElementById("infobody").innerHTML = "Heading to gateway with Ferris and Westley.";
+}
+
+ function hourFourClick() {
+   document.getElementById("infophoto").style.background = "url('https://i.imgur.com/SQil9Kb.jpg')";
+   document.getElementById("infocontainer").style.transform = "translateY(0%)";
+   document.getElementById("infoheader").innerHTML = "HOUR 4";
+   document.getElementById("infobody").innerHTML = "Dean and Westley's room window.";
+ }
+
+ function hourFiveClick() {
+   document.getElementById("infophoto").style.background = "url('https://i.imgur.com/GLelqeA.jpg')";
+   document.getElementById("infocontainer").style.transform = "translateY(0%)";
+   document.getElementById("infoheader").innerHTML = "HOUR 5";
+   document.getElementById("infobody").innerHTML = "Dean being angry at his computer.</br></br>Nothing new.";
+ }
+
+ function hourSixClick() {
+   document.getElementById("infophoto").style.background = "url('https://i.imgur.com/NW0nbmo.jpg')";
+   document.getElementById("infocontainer").style.transform = "translateY(0%)";
+   document.getElementById("infoheader").innerHTML = "HOUR 6";
+   document.getElementById("infobody").innerHTML = "Getting on the elevator back at home.";
+ }
+
+ function hourSevenClick() {
+   document.getElementById("infophoto").style.background = "url('https://i.imgur.com/j0yAe8W.jpg')";
+   document.getElementById("infocontainer").style.transform = "translateY(0%)";
+   document.getElementById("infoheader").innerHTML = "HOUR 7";
+   document.getElementById("infobody").innerHTML = "Making some music.";
+ }
+
+ function hourEightClick() {
+   document.getElementById("infophoto").style.background = "url('https://i.imgur.com/tfG83CK.jpg')";
+   document.getElementById("infocontainer").style.transform = "translateY(0%)";
+   document.getElementById("infoheader").innerHTML = "HOUR 8";
+   document.getElementById("infobody").innerHTML = "Sun starting to set.";
+ }
+
+ function hourNineClick() {
+   document.getElementById("infophoto").style.background = "url('https://i.imgur.com/illLsuM.jpg')";
+   document.getElementById("infocontainer").style.transform = "translateY(0%)";
+   document.getElementById("infoheader").innerHTML = "HOUR 9";
+   document.getElementById("infobody").innerHTML = "At the photo lab doing some prints. For this project I went out and got some tires and went around setting them up like they were an actual car.";
+ }
+
+ function hourTenClick() {
+   document.getElementById("infophoto").style.background = "url('https://i.imgur.com/2z9kSgl.jpg')";
+   document.getElementById("infocontainer").style.transform = "translateY(0%)";
+   document.getElementById("infoheader").innerHTML = "HOUR 10";
+   document.getElementById("infobody").innerHTML = "The prints came out great. The photos are pretty funny and I got a good one of Ferris and Dean standing in them like passengers.";
+ }
+
+ function hourElevenClick() {
+   document.getElementById("infophoto").style.background = "url('https://i.imgur.com/PwYXIh2.jpg')";
+   document.getElementById("infocontainer").style.transform = "translateY(0%)";
+   document.getElementById("infoheader").innerHTML = "HOUR 11";
+   document.getElementById("infobody").innerHTML = "My front door, arriving home.";
+ }
+
+ function hourTwelveClick() {
+   document.getElementById("infophoto").style.background = "url('https://i.imgur.com/kEteASS.jpg')";
+   document.getElementById("infocontainer").style.transform = "translateY(0%)";
+   document.getElementById("infoheader").innerHTML = "HOUR 12";
+   document.getElementById("infobody").innerHTML = "Working on a booklet for typography.";
+ }
